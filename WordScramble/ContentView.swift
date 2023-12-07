1976bcbed26fac778892b441312e360ef338d11b
mexicola84 <i@jan-grimm.com>
2023-12-07 14:43:50 +0100

    Finished day 31 challenge of 100 days of SwiftUI by implementing another guard methods, a toolbar button to restart game and a score view which calculates the score based on word count and letter count per word.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -16,12 +16,19 @@ struct ContentView: View {
     @State private var errorMessage = ""
     @State private var showingError = false
     
+    //day 31 challenge part 3: add a score to the view.
+    @State private var score = 0
+    
     var body: some View {
         NavigationStack {
             List {
                 Section {
                     TextField("Enter your word", text: $newWord)
-                        .textInputAutocapitalization(.never)  //not automaticially capitalize the first letter
+                        .textInputAutocapitalization(.never) //not automaticially capitalize the first letter
+                }
+                
+                Section {
+                    Text("Your current score is: \(score)")
                 }
                 
                 Section {
@@ -34,6 +41,13 @@ struct ContentView: View {
                 }
             }
             .navigationTitle(rootWord)
+            // day 31 challenge part 2: built a toolbar button to start a new game whenever you want.
+            .toolbar{
+                Button("Start new Game") {
+                    startGame()
+                    score = 0
+                }
+            }
             .onSubmit(addNewWord) // submit the word into the usedwords array by submitting the textfield with return
             .onAppear(perform: startGame) // run a method when view is loaded
             .alert(errorTitle, isPresented: $showingError) {
@@ -46,9 +60,20 @@ struct ContentView: View {
     
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        let scoreMultiplier = answer.count //implement the letter count per answer as a base for the score.
         
         // error handling for inputing new words.
-        guard answer.count > 0 else { return }
+        
+        //day 31 challenge part 1: Error when newWord == rootWord, minimum letters per new Word should be three.
+        guard answer.count > 2 else {
+            wordError(title: "Entry is too short", message: "Your input word is too short. Make sure your new word has at least three letters.")
+            return
+        }
+        
+        guard answer != rootWord else {
+            wordError(title: "Same as Root Word", message: "You have to build a new word out of the letters of \(rootWord)")
+            return
+        }
         
         guard isOriginal(word: answer) else {
             wordError(title: "Word used already", message: "Be more original!")
@@ -69,6 +94,7 @@ struct ContentView: View {
         withAnimation {
             //insert the new word on top of the list, that's why there's no use of .append
             usedWords.insert(answer, at: 0)
+            score += 10 * scoreMultiplier //add to score.
             newWord = ""
         }
     }
